cadb2d555d6aa7a922307d8da59e696eb39cf0f4
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-04-25 12:47:20 +0530

    render the products list in js file.

--- Amazon-Clone/scripts/amazon.js
+++ Amazon-Clone/scripts/amazon.js
@@ -1,9 +1,8 @@
-
-// product items list array
+// product product list array
 
 const products = [
   {
-    images: "../images/products/athletic-cotton-socks-6-pairs.jpg",
+    images: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
       stars: 4.5,
@@ -12,7 +11,7 @@ const products = [
     price: 10090,
   },
   {
-    images: "../images/products/intermediate-composite-basketball.jpg",
+    images: "images/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
     rating: {
       stars: 4.0,
@@ -21,7 +20,7 @@ const products = [
     price: 20095,
   },
   {
-    images: "../images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    images: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Adults Plain Cotton T-shirt",
     rating: {
       stars: 4.5,
@@ -30,3 +29,29 @@ const products = [
     price: 799,
   },
 ];
+
+// loop through the product
+
+function renderItemsList() {
+  let getListItems = "";
+  products.forEach((product, index) => {
+    let html = `
+        <div key=${index}>
+            <div class="products-list">
+                <ul>
+                <li><img src="${product.images}" alt="product-images" /></li>
+                <li>${product.name}</li>
+                <li>${product.rating.stars}</li>
+                <li>${product.rating.count}</li>
+                <li>${product.price}</li>
+                </ul>
+            </div>
+        </div>
+    `;
+    getListItems += html;
+  });
+
+  document.querySelector(".js-products-grid").innerHTML = getListItems;
+}
+
+renderItemsList();
