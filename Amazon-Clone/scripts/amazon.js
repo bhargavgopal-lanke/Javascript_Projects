5c2760a0ce2aaf57ee52a45cee191b7fec38208b
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-04-25 13:17:35 +0530

    UI updated for tha app.

--- Amazon-Clone/scripts/amazon.js
+++ Amazon-Clone/scripts/amazon.js
@@ -35,20 +35,58 @@ const products = [
 function renderItemsList() {
   let getListItems = "";
   products.forEach((product, index) => {
-    let html = `
-        <div key=${index}>
-            <div class="products-list">
-                <ul>
-                <li><img src="${product.images}" alt="product-images" /></li>
-                <li>${product.name}</li>
-                <li>${product.rating.stars}</li>
-                <li>${product.rating.count}</li>
-                <li>${product.price}</li>
-                </ul>
+    let productsHtml = `
+        <div class="product-container" key="${index}">
+            <div class="product-image-container">
+            <img class="product-image"
+                src="${product.images}">
             </div>
+
+            <div class="product-name limit-text-to-2-lines">
+                  ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+            <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+                ${product.rating.count}
+            </div>
+            </div>
+
+            <div class="product-price">
+                $${(product.price) / 100 }
+            </div>
+
+            <div class="product-quantity-container">
+            <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+                <img src="images/checkmark.png">
+                Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
+            Add to Cart
+            </button>
         </div>
     `;
-    getListItems += html;
+    getListItems += productsHtml;
   });
 
   document.querySelector(".js-products-grid").innerHTML = getListItems;
