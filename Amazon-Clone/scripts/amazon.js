ddb89fed6c814824b51b44e5f3e636fcfa25f072
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-04-25 23:33:41 +0530

    pushing items to the cart and displaying cart item value in the cart icon.

--- Amazon-Clone/scripts/amazon.js
+++ Amazon-Clone/scripts/amazon.js
@@ -7,11 +7,11 @@
 // 4. the purpose of data attribute is to attach any information to an element
 // data-product-name=${product.name} => name and value pair
 
-// below is the accumulator pattern we loop through the array add add the result to the variable
+// below is the accumulator pattern we loop through an array add add the result to the variable
 let getListItems = "";
 products.forEach((product, index) => {
   let productsHtml = `
-        <div class="product-container" key="${index}">
+        <div class="product-container"  key="${index}">
             <div class="product-image-container">
             <img class="product-image"
                 src="${product.image}">
@@ -56,7 +56,7 @@ products.forEach((product, index) => {
             </div>
 
             <button class="add-to-cart-button button-primary js-add-to-cart"
-           data-product-name="${product.name}">
+           data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>
@@ -67,19 +67,34 @@ products.forEach((product, index) => {
 document.querySelector(".js-products-grid").innerHTML = getListItems;
 
 // this will give the list of all the add to card buttons in the page.
-let count = 1;
-document
-  .querySelectorAll(".js-add-to-cart")
-  .forEach((addToCartButton, index) => {
-    // console.log("addToCartButton.dataset", addToCartButton);
-    addToCartButton.addEventListener("click", () => {
-      const productName = addToCartButton.dataset.productName;
-      const countValue = document.querySelector(".js-cart-quantity").innerHTML = count++;
-      Cart.push({
-         productName,
-         countValue,
-      });
+let cartQuantity = 1;
+document.querySelectorAll(".js-add-to-cart").forEach((addToCartButton) => {
+  // console.log("addToCartButton.dataset", addToCartButton);
+  addToCartButton.addEventListener("click", () => {
+    // selected productname value we're getting it by using dataset
+    // dataset convert from kebab case(product-id) to camelCase(productID)
+    const productId = addToCartButton.dataset.productId;
 
-      console.log(Cart);
+    let matchingItem;
+
+    // looping through all the cart items and comparing the selected productname with existing items product name
+    Cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
     });
+
+    // if the matching item is true increasing the quantity of the item object
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      Cart.push({
+        productId,
+        quantity: 1,
+      });
+    }
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity++;
   });
+});
