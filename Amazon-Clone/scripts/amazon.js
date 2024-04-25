0495abc71a35b7a20501839cbb31978f51ca75c5
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-04-25 12:36:24 +0530

    Some items crated in the products array.

---
+++ Amazon-Clone/scripts/amazon.js
@@ -0,0 +1,32 @@
+
+// product items list array
+
+const products = [
+  {
+    images: "../images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    price: 10090,
+  },
+  {
+    images: "../images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4.0,
+      count: 127,
+    },
+    price: 20095,
+  },
+  {
+    images: "../images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-shirt",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    price: 799,
+  },
+];
