dd274c84e95062cddbaaefb14e8722cca9aab878
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-04-28 16:27:00 +0530

    remove the item functionality added.

--- Amazon-Clone/scripts/checkout.js
+++ Amazon-Clone/scripts/checkout.js
@@ -15,85 +15,85 @@ function productsList(productId) {
 let checkoutItems;
 function generateHtml(matchingProduct, productQuantity) {
   checkoutItems += `
-  <div class="cart-item-container">
-  <div class="delivery-date">
-    Delivery date: Tuesday, June 21
-  </div>
-
-  <div class="cart-item-details-grid">
-    <img class="product-image"
-      src="${matchingProduct.image}">
-
-    <div class="cart-item-details">
-      <div class="product-name">
-        ${matchingProduct.name}
-      </div>
-      <div class="product-price">
-      ${formatCurrency(matchingProduct.priceCents)}
-      </div>
-      <div class="product-quantity">
-        <span>
-          Quantity: <span class="quantity-label">${productQuantity}</span>
-        </span>
-        <span class="update-quantity-link link-primary">
-          Update
-        </span>
-        <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
-          matchingProduct.id
-        }">
-          Delete
-        </span>
-      </div>
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    <div class="delivery-date">
+      Delivery date: Tuesday, June 21
     </div>
 
-    <div class="delivery-options">
-      <div class="delivery-options-title">
-        Choose a delivery option:
+    <div class="cart-item-details-grid">
+      <img class="product-image"
+        src="${matchingProduct.image}">
+
+      <div class="cart-item-details">
+        <div class="product-name">
+          ${matchingProduct.name}
+        </div>
+        <div class="product-price">
+        ${formatCurrency(matchingProduct.priceCents)}
+        </div>
+        <div class="product-quantity">
+          <span>
+            Quantity: <span class="quantity-label">${productQuantity}</span>
+          </span>
+          <span class="update-quantity-link link-primary">
+            Update
+          </span>
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+            matchingProduct.id
+          }">
+            Delete
+          </span>
+        </div>
       </div>
-      <div class="delivery-option">
-        <input type="radio" checked
-          class="delivery-option-input"
-          name="delivery-option-${matchingProduct.id}">
-        <div>
-          <div class="delivery-option-date">
-            Tuesday, June 21
+
+      <div class="delivery-options">
+        <div class="delivery-options-title">
+          Choose a delivery option:
+        </div>
+        <div class="delivery-option">
+          <input type="radio" checked
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">
+              Tuesday, June 21
+            </div>
+            <div class="delivery-option-price">
+              FREE Shipping
+            </div>
           </div>
-          <div class="delivery-option-price">
-            FREE Shipping
+        </div>
+        <div class="delivery-option">
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">
+              Wednesday, June 15
+            </div>
+            <div class="delivery-option-price">
+              $4.99 - Shipping
+            </div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">
+              Monday, June 13
+            </div>
+            <div class="delivery-option-price">
+              $9.99 - Shipping
+            </div>
           </div>
         </div>
       </div>
-      <div class="delivery-option">
-        <input type="radio"
-          class="delivery-option-input"
-          name="delivery-option-${matchingProduct.id}">
-        <div>
-          <div class="delivery-option-date">
-            Wednesday, June 15
-          </div>
-          <div class="delivery-option-price">
-            $4.99 - Shipping
-          </div>
-        </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio"
-          class="delivery-option-input"
-          name="delivery-option-${matchingProduct.id}">
-        <div>
-          <div class="delivery-option-date">
-            Monday, June 13
-          </div>
-          <div class="delivery-option-price">
-            $9.99 - Shipping
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+   </div>
 </div>
 
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: Wednesday, June 15
   </div>
@@ -108,15 +108,17 @@ cartData.forEach((cartItems) => {
   productsList(productId);
   generateHtml(matchingProduct, productQuantity);
   document.querySelector(".order-summary").innerHTML = checkoutItems;
-  console.log("productListItem", matchingProduct);
 });
 
 
-  document.querySelectorAll(".js-delete-link").forEach((link) => {
+  document.querySelectorAll(".js-delete-link").forEach((link, index) => {
     link.addEventListener("click", () => {
       // the product we're trying to remove
       const productId = link.dataset.productId;
       removeFromCart(productId);
+      const [container, containerOne] = document.querySelectorAll(`.js-cart-item-container-${productId}`);
+      container.remove();
+      containerOne.remove();
     });
   });
 
--- Amazon-Clone/scripts/utils/utils.js
+++ Amazon-Clone/scripts/utils/utils.js
@@ -1,3 +1,7 @@
 export function formatCurrency(priceCents) {
   return (priceCents / 100).toFixed(2);
 }
+
+export function generateRandomDate() {
+
+}
