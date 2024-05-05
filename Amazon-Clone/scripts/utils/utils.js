954111bcb10217277ee53b1a8bc396b9008a88a6
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-05-05 22:17:53 +0530

    generate random date moved to another function.

--- Amazon-Clone/scripts/utils/utils.js
+++ Amazon-Clone/scripts/utils/utils.js
@@ -2,7 +2,9 @@ export function formatCurrency(priceCents) {
   return (priceCents / 100).toFixed(2);
 }
 
-export function generateRandomDate() {
-  const date = Date();
-  console.log("date", date());
+export function generateRandomDate(deliveryOption) {
+  const today = dayjs(); // get todays date
+  const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+  const dateString = deliveryDate.format("dddd, MMMM D");
+  return dateString;
 }
