0e1498b8d51f7f205a938533d7dd16cb02b037ff
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-05-26 16:51:01 +0530

    code optimization.

--- Amazon-Clone/data/deliveryOptions.js
+++ Amazon-Clone/data/deliveryOptions.js
@@ -15,3 +15,16 @@ export const deliveryOptions = [
     priceCents: 999,
   },
 ];
+
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if(option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+}
+
