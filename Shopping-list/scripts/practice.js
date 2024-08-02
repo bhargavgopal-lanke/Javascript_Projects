1520a8c5d73fb37efcf19c742778e7b32d107f7e
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-08-02 16:38:41 +0530

    do something after every 5 secs.

--- Shopping-list/scripts/practice.js
+++ Shopping-list/scripts/practice.js
@@ -3,22 +3,41 @@ const itemList = document.querySelector(".item-list");
 
 const items = document.querySelectorAll("li");
 
-function run() {
-  //   console.log(1);
-  // add className to the paragraph
-  //   text.className = "dark card";
+// function run() {
+//   console.log(1);
+// add className to the paragraph
+//   text.className = "dark card";
 
-  //   itemList.classList.forEach((c) => console.log(c));
-  // add the class on click of the button
-  text.classList.add("dark");
-  // toggle the class on click of the button
-  text.classList.toggle("hidden");
-  // replace the existing class with new class
-  text.classList.replace("dark", "card");
-  // change style
-  itemList.style.lineHeight = "3";
-}
+//   itemList.classList.forEach((c) => console.log(c));
+// add the class on click of the button
+text.classList.add("dark");
+// toggle the class on click of the button
+text.classList.toggle("hidden");
+// replace the existing class with new class
+text.classList.replace("dark", "card");
+// change style
+itemList.style.lineHeight = "3";
+
+items.forEach((items, index) => {
+  items.style.color = "red";
+  if (index === 2) {
+    items.style.color = "green";
+    items.style.fontWeight = "bolder";
+    items.style.fontSize = "18px";
+  }
+});
+// }
 
 // get the button from the dom and assign the function to the event listener
 // when we click on this button add the class to the paragraph
 // document.querySelector("button").onclick = run;
+
+// do something after every 5 secs
+
+function run() {
+  alert("Alert the msg");
+}
+
+setTimeout(() => {
+  run();
+}, 5000);
