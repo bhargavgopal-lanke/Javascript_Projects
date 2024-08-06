60e5c3a9f6aa2979bea78ae55da69e0cd4390a4f
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-08-06 17:35:11 +0530

    input fields values fetching from checkbox dropdown and inputfield using input, change and focus, blur event handlers.

--- Shopping-list/scripts/index2.js
+++ Shopping-list/scripts/index2.js
@@ -1,2 +1,36 @@
-let inputFieldValue = document.querySelector("#item-value").value;
+let inputField = document.querySelector("#item-value");
 let displayValueField = document.querySelector("#entered-value");
+let priorityInput = document.querySelector("#priority-input");
+let checkbox = document.querySelector("#checkbox");
+let heading1 = document.querySelector("h1");
+
+function onInput(e) {
+  heading1.textContent = e.target.value;
+}
+
+function onChecked(e) {
+  console.log("checked boolean", e.target.checked);
+  const checkedValue = e.target.checked;
+  const isChecked = checkedValue ? "checked" : "notChecked";
+  console.log("isChecked", isChecked);
+}
+
+function onFocus() {
+  console.log("Input is focused");
+  inputField.style.outline = "5px solid green";
+}
+
+function onBlur() {
+  console.log("Input is Blur");
+  inputField.style.backgroundColor = "red";
+}
+
+checkbox.addEventListener("click", (e) => {
+  onChecked(e);
+});
+
+inputField.addEventListener("input", onInput);
+priorityInput.addEventListener("change", onInput);
+checkbox.addEventListener("input", onChecked);
+inputField.addEventListener("focus", onFocus);
+inputField.addEventListener("blur", onBlur);
