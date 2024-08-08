ff8a6b0972d0eadcd2475e80a11bcc52c6e62e11
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-08-08 14:40:58 +0530

    basic sample code.

---
+++ MaxMuller-Js/Functions/functionSample.js
@@ -0,0 +1,25 @@
+const start = function startGame() {
+  console.log("game is starting");
+};
+
+const person = {
+  name: "max",
+  greet: function () {
+    console.log("hello there");
+  },
+};
+
+startGameBtn.addEventListener("click", start);
+// function declaration / function statement
+// it can be hoisted to the top, can be declared anywhere in the file.
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// function expression
+// hoisted to the top but not initialized/defined (i.e: not after it's used.)
+// multiply();  Cannot access multiply before initilization.
+const multiply = function (a, b) {
+  return a * b;
+};
