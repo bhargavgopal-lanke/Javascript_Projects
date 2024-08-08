1d3241e521e2c7bea63224d5a3bca974c7c1e39b
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-08-08 15:28:18 +0530

    comments added.

--- MaxMuller-Js/Functions/Functions.js
+++ MaxMuller-Js/Functions/Functions.js
@@ -1,17 +1,20 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
+// variables defined outside can be accessed inside an function
 const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
+const DEFAULT_CHOICE = "ROCK";
 
 const getPlayerChoice = function () {
+  // convert the entered values to uppercase using uppercase method.
   const selection = prompt(
-    `${ROCK}, ${PAPER} or ${SCISSORS}`,
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
     ""
   ).toUpperCase();
 
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-    alert("Invalid Choice!");
+    alert(`Invalid Choice! We Chose ${DEFAULT_CHOICE} for you`);
   }
 };
 
