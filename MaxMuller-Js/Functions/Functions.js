2c7a057edaa686404e39ece29a3e58774d9428e4
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-08-08 14:52:56 +0530

    variables decalred outside can be accessed inside a function.

--- MaxMuller-Js/Functions/Functions.js
+++ MaxMuller-Js/Functions/Functions.js
@@ -1,4 +1,18 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
 
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}`,
+    ""
+  ).toUpperCase();
 
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert("Invalid Choice!");
+  }
+};
+
+startGameBtn.addEventListener("click", getPlayerChoice);
