6de734fc50b661c44734b6a21d843106c8a479f2
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-08-11 08:37:12 +0530

    get the winner based on the computer choiceand the player choice.

--- MaxMuller-Js/Functions/Functions.js
+++ MaxMuller-Js/Functions/Functions.js
@@ -5,7 +5,11 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_CHOICE = "ROCK";
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
+// the playyer choice
 const getPlayerChoice = function () {
   // convert the entered values to uppercase using uppercase method.
   const selection = prompt(
@@ -19,6 +23,40 @@ const getPlayerChoice = function () {
   }
   return selection;
 };
+
+// get the winner
+
+function getWinner(pChoice, cChoice) {
+  if (pChoice === cChoice) {
+    return RESULT_DRAW;
+  } else if (
+    cChoice === ROCK && pChoice === PAPER ||
+    cChoice === PAPER && pChoice === SCISSORS ||
+    cChoice === SCISSORS && pChoice === ROCK
+  ) {
+    return RESULT_PLAYER_WINS
+  } else {
+    return RESULT_COMPUTER_WINS
+  }
+}
+
+//  the computer choice
+function getComputerChoice() {
+  const randomValue = Math.floor(Math.random() * 10);
+  let computerResult;
+
+  if (randomValue <= 3) {
+    computerResult = ROCK;
+  } else if (randomValue <= 6) {
+    computerResult = PAPER;
+  } else {
+    computerResult = SCISSORS;
+  }
+  console.log("randomValue", computerResult);
+  return computerResult;
+}
+
+getComputerChoice();
 
 startGameBtn.addEventListener("click", () => {
   let playerChoiceResult = getPlayerChoice();
