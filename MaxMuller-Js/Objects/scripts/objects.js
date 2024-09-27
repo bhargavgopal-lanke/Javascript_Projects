76144e0f7fc7f9a08ff2c12047861d7d01ca4530
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-09-27 23:32:06 +0530

    adding the visible class based on the movie list.

--- MaxMuller-Js/Objects/scripts/objects.js
+++ MaxMuller-Js/Objects/scripts/objects.js
@@ -5,8 +5,18 @@ const searchButton = document.getElementById("serach-btn");
 const movies = [];
 
 const renderMovies = () => {
-  const movieList = document.getElementById('movie-list');
-}
+  const movieList = document.getElementById("movie-list");
+  movieList.innerHTML = "";
+  // if there are no movies execute below code and remove the classname visible from movie-list div
+  // if we have movies in the movielist add class visible
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+  } else {
+    movieList.classList.add("visible");
+  }
+};
+
+renderMovies();
 
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
