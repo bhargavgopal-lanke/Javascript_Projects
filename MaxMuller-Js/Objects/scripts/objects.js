8b522660560dd7ec2614850510c5465bac33ef5e
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-09-28 00:02:51 +0530

    rendering the title from the movies object

--- MaxMuller-Js/Objects/scripts/objects.js
+++ MaxMuller-Js/Objects/scripts/objects.js
@@ -1,4 +1,4 @@
-const addMoviebutton = document.getElementById("add-movvie-btn");
+const addMoviebutton = document.getElementById("add-movie-btn");
 
 const searchButton = document.getElementById("serach-btn");
 
@@ -6,17 +6,24 @@ const movies = [];
 
 const renderMovies = () => {
   const movieList = document.getElementById("movie-list");
-  movieList.innerHTML = "";
+
   // if there are no movies execute below code and remove the classname visible from movie-list div
   // if we have movies in the movielist add class visible
+
   if (movies.length === 0) {
     movieList.classList.remove("visible");
+    return;
   } else {
     movieList.classList.add("visible");
   }
-};
+  movieList.innerHTML = "";
 
-renderMovies();
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
+};
 
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
@@ -39,5 +46,8 @@ const addMovieHandler = () => {
     id: Math.random() * 10,
   };
 
-  return newMovie;
+  movies.push(newMovie);
+  renderMovies();
 };
+
+addMoviebutton.addEventListener("click", addMovieHandler);
