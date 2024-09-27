281978519cc90def58aa528b351bf0657779df60
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-09-27 23:22:03 +0530

    render the movies list from the objects.

--- MaxMuller-Js/Objects/scripts/objects.js
+++ MaxMuller-Js/Objects/scripts/objects.js
@@ -2,7 +2,32 @@ const addMoviebutton = document.getElementById("add-movvie-btn");
 
 const searchButton = document.getElementById("serach-btn");
 
+const movies = [];
+
+const renderMovies = () => {
+  const movieList = document.getElementById('movie-list');
+}
+
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
-  const extraName = document.getElementById("extra-name");
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random() * 10,
+  };
+
+  return newMovie;
 };
