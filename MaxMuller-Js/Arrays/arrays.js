f067634e12089018216e35c96ac72d6ad3aa2160
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-10-05 17:55:08 +0530

    comment added for splice.

--- MaxMuller-Js/Arrays/arrays.js
+++ MaxMuller-Js/Arrays/arrays.js
@@ -1,8 +1,23 @@
 // array practice
-// the length of an array is alway indexValues - 1  
+// the length of an array is alway indexValues - 1
 // arrays are index based and the first element starts with zero.
 
-const hobbies = ["Cooking", "Sports"];
+const hobbies = ["Sports", "Cooking"];
+
+hobbies.push("Reading");
+hobbies.unshift("Coding");
+const poppedValue = hobbies.pop();
+hobbies.shift();
+
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+
+hobbies.splice(0, 0, 'Good Food');
+// splice without index position will delete all the items for that index position inside an array
+hobbies.splice(0);
+console.log("hobbies", hobbies);
+
 const personalData = [30, "Max", { moreDetail: [] }];
 
 // multidimentional array
@@ -10,9 +25,11 @@ const analyticsData = [
   [1, 1.6],
   [-5.4, 2.1],
 ];
+
 // if the data is a string variable declaration and forloop inside code.
 // let loopData = ''     ===>  loopData +=  `${dataPoint}, `;  ;
 // if the data is getting pushed into an array
+
 let loopData = [];
 for (const data of analyticsData) {
   for (const dataPoint of data) {
