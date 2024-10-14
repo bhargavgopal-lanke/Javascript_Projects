2590fec01a7ecf30514f04f29de1a176fec734b9
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-10-14 05:47:31 +0530

    this in regular functions returns global object and in strict mode it returns undefined.

--- MaxMuller-Js/Objects/scripts/this.js
+++ MaxMuller-Js/Objects/scripts/this.js
@@ -1,7 +1,16 @@
 // 1. this in global context (i.e. outside of any function)
+
 function something() {
   console.log("something");
 }
 
 console.log("this outside of any function", this); // logs global object always (also in strict mode)!
+
+// 2. this in a function (non-arrow) - called in the global context
+
+function somethingOne() {
+  console.log(this);
+}
+
+somethingOne(); // logs global object in non strict mode, undefined in strict mode
 
