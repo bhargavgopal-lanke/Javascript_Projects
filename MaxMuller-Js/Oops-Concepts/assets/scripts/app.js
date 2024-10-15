7876082a7f22e852d632d2254c51e72c4eea4a8f
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-10-15 08:27:35 +0530

    Minor changes in the data.

--- MaxMuller-Js/Oops-Concepts/assets/scripts/app.js
+++ MaxMuller-Js/Oops-Concepts/assets/scripts/app.js
@@ -1,14 +1,14 @@
 const productsList = {
   products: [
     {
-      title: "A wallposter",
+      title: "A Lake Wallposter",
       imageUrl:
         "https://wallpaper.forfun.com/fetch/62/624e27fde335d49e2dd3c6b75c6027a3.jpeg?download=nature-novaya_zelandiya-ostrov-ozero-uanaka-50812.jpeg",
       price: 19.99,
       description: "A great wallposter",
     },
     {
-      title: "japan city",
+      title: "Japan City",
       imageUrl:
         "https://wallpaper.forfun.com/fetch/d9/d99cfc4cec7dad7bedc999a2e23802e5.jpeg?download=anime-doma-goroda-27499.jpeg",
       price: 89.99,
