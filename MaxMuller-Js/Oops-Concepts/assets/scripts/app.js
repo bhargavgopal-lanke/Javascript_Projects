a7e9347658ca0ef8e3d93b26de4e4eb8d98cbbd2
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-10-15 08:11:47 +0530

    rendering the product list in the same product object using render method.

--- MaxMuller-Js/Oops-Concepts/assets/scripts/app.js
+++ MaxMuller-Js/Oops-Concepts/assets/scripts/app.js
@@ -15,5 +15,29 @@ const productsList = {
       description: "A Japan city wallposter",
     },
   ],
-  render() {},
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    // looping through all the elements in the products
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+        <div>
+            <img src="${prod.imageUrl}" alt="${prod.title}" />
+            <div class="product-item__content">
+                <h2>${prod.title}</h2>
+                <h3>\$${prod.price}</h3>
+                <p>${prod.description}</p>
+                <button>Add to cart</button>
+            </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  },
 };
+
+productsList.render();
