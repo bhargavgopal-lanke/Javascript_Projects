af38972ca3ebc214f43ae6600fb9ca3473b2250e
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-10-15 07:41:58 +0530

    products data moved into products list. and added render method.

--- MaxMuller-Js/Oops-Concepts/assets/scripts/app.js
+++ MaxMuller-Js/Oops-Concepts/assets/scripts/app.js
@@ -1,16 +1,19 @@
-const products = [
-  {
-    title: "A wallposter",
-    imageUrl:
-      "https://wallpaper.forfun.com/fetch/62/624e27fde335d49e2dd3c6b75c6027a3.jpeg?download=nature-novaya_zelandiya-ostrov-ozero-uanaka-50812.jpeg",
-    price: 19.99,
-    description: "A great wallposter",
-  },
-  {
-    title: "japan city",
-    imageUrl:
-      "https://wallpaper.forfun.com/fetch/d9/d99cfc4cec7dad7bedc999a2e23802e5.jpeg?download=anime-doma-goroda-27499.jpeg",
-    price: 89.99,
-    description: "A Japan city wallposter",
-  },
-];
+const productsList = {
+  products: [
+    {
+      title: "A wallposter",
+      imageUrl:
+        "https://wallpaper.forfun.com/fetch/62/624e27fde335d49e2dd3c6b75c6027a3.jpeg?download=nature-novaya_zelandiya-ostrov-ozero-uanaka-50812.jpeg",
+      price: 19.99,
+      description: "A great wallposter",
+    },
+    {
+      title: "japan city",
+      imageUrl:
+        "https://wallpaper.forfun.com/fetch/d9/d99cfc4cec7dad7bedc999a2e23802e5.jpeg?download=anime-doma-goroda-27499.jpeg",
+      price: 89.99,
+      description: "A Japan city wallposter",
+    },
+  ],
+  render() {},
+};
